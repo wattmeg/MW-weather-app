9e197413669baded4b38ef90a8057ef9e4700222
wattmeg <114342311+wattmeg@users.noreply.github.com>
2022-11-12 10:03:45 +0000

    reordered/neatened javascript file

--- src/script.js
+++ src/script.js
@@ -24,10 +24,6 @@ function formatDate(timestamp) {
 }
 //end of determine date
 
-//trigger the search
-let triggerSearch = document.querySelector("#search-button");
-triggerSearch.addEventListener("click", searchCity);
-
 //search for city, temp in API
 function searchCity(event) {
   event.preventDefault();
@@ -67,37 +63,6 @@ function showResults(response) {
   //oldMinMax.innerHTML = `Min temp is ${newMin}, max temp is ${newMax}`;
 }
 
-//show fahrenheit
-function showFahrenheit(event) {
-  event.preventDefault();
-  let fahrenheitTemperature = document.querySelector("#current-temp");
-  celsiusTemp.classList.remove("active");
-  fahrenheitTemp.classList.add("active");
-  let newFahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
-  fahrenheitTemperature.innerHTML = Math.round(newFahrenheitTemperature);
-}
-
-let fahrenheitTemp = document.querySelector("#temp-fahrenheit-link");
-fahrenheitTemp.addEventListener("click", showFahrenheit);
-// end of show fahrenheit
-
-// show celsius
-function showCelsius(event) {
-  event.preventDefault();
-  let newCelsiusTemperature = document.querySelector("#current-temp");
-  newCelsiusTemperature.innerHTML = Math.round(
-    response.data.temperature.current
-  );
-  celsiusTemp.classList.add("active");
-  fahrenheitTemp.classList.remove("active");
-}
-
-let celsiusTemp = document.querySelector("#temp-celsius-link");
-celsiusTemp.addEventListener("click", searchCity);
-// end of show celsius
-
-let celsiusTemperature = null;
-
 //my location button search
 function getLocation(event) {
   event.preventDefault();
@@ -136,5 +101,40 @@ function showLocals(response) {
   //oldMinMax.innerHTML = `Min temp is ${newMin}, max temp is ${newMax}`;
 }
 
+//show fahrenheit
+function showFahrenheit(event) {
+  event.preventDefault();
+  let fahrenheitTemperature = document.querySelector("#current-temp");
+  celsiusTemp.classList.remove("active");
+  fahrenheitTemp.classList.add("active");
+  let newFahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+  fahrenheitTemperature.innerHTML = Math.round(newFahrenheitTemperature);
+}
+// end of show fahrenheit
+
+// show celsius
+function showCelsius(event) {
+  event.preventDefault();
+  let newCelsiusTemperature = document.querySelector("#current-temp");
+  newCelsiusTemperature.innerHTML = Math.round(
+    response.data.temperature.current
+  );
+  celsiusTemp.classList.add("active");
+  fahrenheitTemp.classList.remove("active");
+}
+// end of show celsius
+
+// GLOBAL VARIABLES
+let triggerSearch = document.querySelector("#search-button");
+triggerSearch.addEventListener("click", searchCity);
+
 let myLocation = document.querySelector("#my-location-button");
 myLocation.addEventListener("click", getLocation);
+
+let celsiusTemperature = null;
+
+let celsiusTemp = document.querySelector("#temp-celsius-link");
+celsiusTemp.addEventListener("click", searchCity);
+
+let fahrenheitTemp = document.querySelector("#temp-fahrenheit-link");
+fahrenheitTemp.addEventListener("click", showFahrenheit);
